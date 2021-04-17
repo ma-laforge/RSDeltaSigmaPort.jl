7a150a42ef28b54801cc198cafe0a184f4800b60
MA Laforge <ma.laforge.49@gmail.com>
2021-04-17 15:38:52 -0400

    Add new power calcs.

--- src/power.jl
+++ src/power.jl
@@ -2,17 +2,20 @@
 #-------------------------------------------------------------------------------
 
 """`dbm(v,R=50) = 10*log10(v^2/R*1000)`
-The equivalent in dBm of an rms voltage v
+
+The equivalent in dBm of an RMS voltage v
 """
 dbm(v; R::Float64=50.0) = 10*log10.(abs.(v .^ 2)/R) .+ 30;
 
 """`dbp(x) = 10*log10(x)`
-the dB equivalent of the power x
+
+The dB equivalent of the power x
 """
 dbp(x) = 10*log10.(abs.(x))
 
 """`dbv(x) = 20*log10(abs(x))`
-the dB equivalent of the voltage x
+
+The dB equivalent of the voltage x
 """
 dbv(x) = 20*log10.(abs.(x))
 
@@ -23,5 +26,23 @@ function rms(x; no_dc::Bool=false)
 	end
 	return norm(x)/sqrt(length(x))
 end
+
+"""`v = undbm(p, z=50) = sqrt(z*10^(p/10-3))`
+
+RMS voltage equivalent to a power p in dBm
+"""
+undbm(p; z::Float64=50.0) = sqrt.(z*10 .^ (p/10 .- 3))
+
+"""`y = undbp(x)`
+
+Convert x from dB to a power
+"""
+undbp(x) = 10 .^ (x/10)
+
+"""`y = undbv(x)`
+
+Convert x from dB to a voltage
+"""
+undbv(x) = 10 .^ (x/20)
 
 #Last line
